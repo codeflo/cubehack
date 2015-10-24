9ca3b34b19f67ad29f09b77be40e23d18da83976
Florian Häglsperger <florian@haeglsperger.com>
2015-10-24 18:04:56 +0200

    Add (empty) unit test project

---
+++ source/CubeHack.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿// Copyright (c) the CubeHack authors. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt in the project root.
+
+using System.Reflection;
+
+[assembly: AssemblyTitle("CubeHack Tests")]
+[assembly: AssemblyDescription("CubeHack unit test project")]
