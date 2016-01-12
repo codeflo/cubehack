9c536d75bdec3fff36bee2714b839856ced5e11d
Florian Häglsperger <florian@haeglsperger.com>
2016-01-12 22:45:06 +0100

    Fixes for the add_license_headers script

--- source/CubeHack.FrontEnd/NativeMethods.cs
+++ source/CubeHack.FrontEnd/NativeMethods.cs
@@ -1,4 +1,7 @@
-﻿namespace CubeHack.FrontEnd
+﻿// Copyright (c) the CubeHack authors. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt in the project root.
+
+namespace CubeHack.FrontEnd
 {
     internal static class NativeMethods
     {
